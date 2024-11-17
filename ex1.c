932fbbf2c477169b41b2df0fac0ef163689b86b0
AmitRothschild1 <amitrot1234@gmail.com>
2024-11-17 20:28:33 +0200

    Task1,Task2,Task3 Added!

--- ex1.c
+++ ex1.c
@@ -19,8 +19,7 @@ int main() {
   scanf("%d",&numTask1);
   printf("please enter the position:");
   scanf("%d",&posTask1);
-  int bitTask1 = (numTask1 >> posTask1);
-  bitTask1 = ((bitTask1 % 2)==1);
+  int bitTask1 = (numTask1 >> posTask1)&1;
   printf("The bit position %d of number %d is: %d",posTask1,numTask1,bitTask1);
 
   // Set bit
@@ -30,12 +29,32 @@ int main() {
   Print the output
   Now make sure it's "off" (equal to 0)
   Print the output */
+  int numTask2;
+  int posTask2;
+  printf("please enter a number:");
+  scanf("%d",&numTask2);
+  printf("please enter the position:");
+  scanf("%d",&posTask2);
+  int numberWith1Task2 = numTask2 | (1 << posTask2);
+  int numberWith0Task2 = numTask2 & ~(1 << posTask2);
+  printf("Number with bit %d set to 1: %d",posTask2,numberWith1Task2);
+  printf("\nNumber with bit %d set to 0: %d",posTask2,numberWith0Task2);
 
   // Toggle bit
   printf("\nToggle bit:\n");
   /*Scan two integers (representing number and a position)
   Toggle the bit in this position
   Print the new number */
+  int numTask3;
+  int posTask3;
+  printf("Please enter a number:\n");
+  scanf("%d", &numTask3);
+  printf("Please enter the position:\n");
+  scanf("%d", &posTask3);
+  int bitTask3 = (numTask3 >> posTask3)&1;
+  int newNumBitoffTask3 = (numTask3 & ~(1<<posTask3)) * bitTask3;
+  int newNumBitOnTask3 = (numTask3 | (1<<posTask3)) * ((~bitTask3)&1);
+  printf("Number with bit %d toggled: %d",posTask3,newNumBitOnTask3+newNumBitoffTask3);
   
   // Even - Odd
   printf("\nEven - Odd:\n");
